aacb711c276754590b341a464731586f14d2c0d4
Unknown <aws140230@utdallas.edu>
2018-10-13 12:23:08 -0500

    Constructable objects now snap to grid
    Refer to GridScript for details on that. It also populates the scene with a customizable amount of basic(Sand)Tiles, and surrounds those with invisible walls so the player cant escape the play area

--- Chillenium2018/Assets/Scripts/Construction/Liftable.cs
+++ Chillenium2018/Assets/Scripts/Construction/Liftable.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Liftable : MonoBehaviour {
 
-
+    public bool isBuildable;
+    public GameObject toBuild;
     public GameObject Carrier;
     public bool carried;
     public Vector3 offset;
@@ -18,22 +19,33 @@ public class Liftable : MonoBehaviour {
         if (carried)
         {
             transform.position = Carrier.transform.position+offset;
+            myUI.SetActive(false);
         }
-        if (near && !myUI.activeSelf)
+        if (near && !carried && !myUI.activeSelf)
         {
             myUI.SetActive(true);
         }
-        else if(!near && myUI.activeSelf)
+        else if(!near && myUI.activeSelf || carried)
         {
             myUI.SetActive(false);
         }
-        if (near) near = false;
+        near = false;
 	}
+
+    //Called by player if this bad boy is buildable and turns him into a corresponding wall segment.
+    public void Build()
+    {
+        if (isBuildable)
+        {
+            Instantiate(toBuild, transform.position, transform.rotation);
+        }
+        else Debug.Log("Build called without being buildable. Why did this happen?");
+    }
 
     private void LateUpdate()
     {
-        if (near && !myUI.activeSelf) myUI.SetActive(true);
-        else if (!near) myUI.SetActive(false);
+        //if (near && !myUI.activeSelf) myUI.SetActive(true);
+        //else if (!near) myUI.SetActive(false);
     }
     private void OnCollisionEnter(Collision coll)
     {
