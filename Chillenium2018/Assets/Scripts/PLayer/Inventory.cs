4e3e0c6e84538eb6556842befe44f8db1a8e3a4e
ressie <shaik.ridah@gmail.com>
2018-10-13 17:43:13 -0500

    code for artifacts

---
+++ Chillenium2018/Assets/Scripts/PLayer/Inventory.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Inventory : MonoBehaviour {
+
+    public enum  Artifacts {test,swordofthesea,tentacleextention,icedaggers,bombs,mines,invisibilitycloak,sandguardian,fogfigurine,duplicitydisk,shiningshield,stunningspectacles,}; //list items here
+    public Artifacts artifact = Artifacts.test; 
+    public Artifacts[] inventory = new Artifacts[5];
+    public int curInventory=0;
+    public GameObject bomb;
+    public Sprite TestSprite;
+
+
+    void Update ()
+    {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                switch (inventory[curInventory])
+                {
+                case Artifacts.test:
+                    //Do a thing
+                    break;
+                case Artifacts.swordofthesea:
+                    var curDirection = transform.InverseTransformDirection(myRB.velocity) + transform.position;
+                    Physics2D.CircleCastAll(PLayerControls.transform.position, 3f, Vector2)
+                    Vector3 vectorToCollider = (collider.transform.position - player.transform.position).Normalize();
+                    // 180 degree arc, change 0 to 0.5 for a 90 degree "pie"
+                    if (Vector3.Dot(vectorToCollider, curDirection)) > 0.5)
+                       {
+                        //Damage the enemy
+                       }
+                    break;
+                case Artifacts.tentacleextention:
+                    var curDirection = transform.InverseTransformDirection(myRB.velocity) + transform.position;
+                    //extend+pull enemy
+                    break;
+                case Artifacts.icedaggers:
+                    var curDirection = transform.InverseTransformDirection(myRB.velocity) + transform.position;
+                    Physics2D.CircleCastAll(PLayerControls.transform.position, 3f, Vector2)
+                    Vector3 vectorToCollider = (collider.transform.position - player.transform.position).Normalize();
+                    // 180 degree arc, change 0 to 0.5 for a 90 degree "pie"
+                    if (Vector3.Dot(vectorToCollider, curDirection)) > 0.5)
+                       {
+                        //Freeze the enemy for 6 seconds
+                       }
+                    break;
+                case Artifacts.bombs:
+                   Instantiate(Bomb)
+                    //time activated
+                    break;
+                case Artifacts.mines:
+                   //contact activated
+                    break;
+                case Artifacts.sandguardian:
+                    //Do a thing
+                    break;
+                case Artifacts.fogfigurine:
+                    //Do a thing
+                    break;
+                case Artifacts.duplicitydisk:
+                    //Do a thing
+                    break;
+                case Artifacts.shiningshield:
+                    //Do a thing
+                    break;
+                case Artifacts.stunningspectacles:
+                    //Do a thing
+                    break;
+                default:
+                        break;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                curInventory--;
+                if (curInventory < 0) curInventory = 4;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                curInventory++;
+                if (curInventory > 4) curInventory = 0;
+            }
+        }
+}
---
+++ Chillenium2018/Assets/Scripts/PLayer/InventoryController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inven : MonoBehaviour {
+
+    public GameObject[] InvSlotsUI = new GameObject[5];
+
+    // Update is called once per frame
+    public void UpdateSlot(int slotNum, Sprite invSprite)
+    {
+        //InvSlotsUI[slotNum].GetComponent<Text>().text = invName;
+        InvSlotsUI[slotNum].GetComponent<Image>().sprite = TestSprite;
+    }
+}
